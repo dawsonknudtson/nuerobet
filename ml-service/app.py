f3e5a5337609ea1861a127abf19d8a5253947d4e
Dawson Knudtson <knudtdaw0000@gmail.com>
2025-04-25 15:25:20 -0500

    inital structure

---
+++ ml-service/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'ML Service is running'})
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True) 
